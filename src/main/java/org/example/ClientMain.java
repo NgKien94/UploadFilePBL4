036dded9e46891dc74aefcecc675ee43746e4ce0
NgKien94 <quangkienofqn@gmail.com>
2024-10-19 20:52:02 +0700

    Read file from server

--- src/main/java/org/example/ClientMain.java
+++ src/main/java/org/example/ClientMain.java
@@ -1,4 +1,39 @@
 package org.example;
 
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
 public class ClientMain {
+    public static void main(String[] args) {
+        try{
+            Socket socket = new Socket("localhost",9999);
+
+            InputStream inputStream = socket.getInputStream();
+           // DataInputStream dataInputStream = new DataInputStream(inputStream);
+
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+            byte[] buffer = new byte[4096];
+            int byteLength;
+            while( (byteLength = inputStream.read(buffer)) > 0){
+                System.out.println("Vao vong lap");
+                byteArrayOutputStream.write(buffer,0,byteLength);
+            }
+            String fileContent = byteArrayOutputStream.toString("UTF-8");
+            System.out.println("Nội dung tệp: ");
+            System.out.println(fileContent);
+
+
+
+            // Đóng các luồng và socket
+            byteArrayOutputStream.close();
+            inputStream.close();
+            socket.close();
+
+        }catch (Exception e){
+           System.out.println("Error from client " + e.getMessage());
+        }
+    }
 }
--- src/main/java/org/example/ServerMain.java
+++ src/main/java/org/example/ServerMain.java
@@ -1,7 +1,42 @@
 package org.example;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class ServerMain {
     public static void main(String[] args) {
-        System.out.println("Hello - Upload file");
+        String filePath = "D:\\Video Android\\testFileTCP.txt";
+       try{
+            ServerSocket serverSocket = new ServerSocket(9999);
+            System.out.println("Server created. Waiting for client.......");
+
+            Socket socket = serverSocket.accept();
+           System.out.println("Client connected");
+
+           BufferedInputStream serverRead = new BufferedInputStream(new FileInputStream(filePath));
+
+           // Tao outputStream de gui du lieu di
+           OutputStream outputStream = socket.getOutputStream();
+
+           // DataOutputStream được sử dụng để tự chuyển các kieu nguyên thủy vào outputstream
+
+           DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+          byte[] buffer = new byte[4096];
+          int byteLength ;
+          while((byteLength = serverRead.read(buffer)) > 0){
+              outputStream.write(buffer,0,byteLength);
+          }
+           System.out.println("Gửi tệp hoàn tất.");
+
+
+        serverRead.close();
+        outputStream.close();
+
+       }catch(Exception e){
+           System.out.println("Error from server" +e.getMessage());
+           System.out.println("Cause from server" +e.getCause());
+       }
     }
 }
